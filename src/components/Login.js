2d8d333f4d9ac29c5aaeda8ef631fba5cf63b1b7
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-10-09 16:18:52 -0500

    Add login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,62 @@
 import React, { Fragment, useState, useContext } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext.js";
+import "../style/Login.scss";
 
 export const LoginPage = () => {
+  const history = useHistory();
+  const { register, errors, handleSubmit } = useForm();
+  const { setUserInfo } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNavigation = (e) => {
+    e.preventDefault();
+    if (email === "admin" && password === "test") {
+      setUserInfo(email);
+      history.push("/list");
+    } else {
+      alert("Password or email is invalid");
+    }
+  };
+
   return (
-    <>
-      <h1>LoginPage</h1>
-    </>
+    <Fragment>
+      <div className="wrapper">
+        <div className="title">Login</div>
+
+        <form className="form" onSubmit={handleNavigation}>
+          <div className="inputfield">
+            <label>Email</label>
+            <input
+              name="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="input"
+              placeholder="Ingrese email"
+            ></input>
+            <span className="text-danger ">{errors?.email?.message}</span>
+          </div>
+          <div className="inputfield">
+            <label>Password</label>
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input"
+              placeholder="Ingrese el password"
+            ></input>
+            <span className="text-danger">{errors?.name?.message}</span>
+          </div>
+
+          <div className="inputfield">
+            <input type="submit" value="Registrarse" className="btn"></input>
+          </div>
+        </form>
+      </div>
+    </Fragment>
   );
 };
-
-export default LoginPage;
