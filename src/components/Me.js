a8423bfc1b97bf7fc6f203d13add88c3cff79bea
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-10-10 09:20:23 -0500

    Integrate personal information with component Me

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useStore } from "react-redux";
 import * as FetchMe from "../services/FetchPersonalData.js";
 
-import * as obj from "../data/DataMe"
+import * as obj from "../data/DataMe";
 export const Me = () => {
   const store = useStore();
   const token = store.getState().reducer_token.token.access_token;
+  const [personalData, setPersonalData] = useState("");
 
   useEffect(() => {
     let data = async () => {
-      console.log(obj.personalData)
-      let response = await FetchMe.Me(token);
+      // let response = await FetchMe.Me(token);
+      let response = obj.personalData;
       if (response) {
+        setPersonalData(response);
       }
     };
     data();
@@ -20,7 +22,23 @@ export const Me = () => {
 
   return (
     <div>
-      <h1>Me</h1>
+      <div>
+        <div>
+          <h1>Personal data of {personalData.name} </h1>
+
+          <ul>
+            <li>id: {personalData.id}</li>
+            <li>name: {personalData.name}</li>
+            <li>email: {personalData.email}</li>
+            <li>email_verified_at: {personalData.email_verified_at}</li>
+            <li>created_at: {personalData.created_at}</li>
+            <li>updated_at: {personalData.updated_at}</li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
+
+      <Link to="/list">Navigate back to list</Link>
     </div>
   );
 };
