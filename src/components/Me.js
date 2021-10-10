6dc7e497339942f7f74cd68bc3b719d69793cb28
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-10-10 08:45:17 -0500

    Fetching data in me's component

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useStore } from "react-redux";
+import * as FetchMe from "../services/FetchPersonalData.js";
 
 export const Me = () => {
+  const store = useStore();
+  const token = store.getState().reducer_token.token.access_token;
+
+  useEffect(() => {
+    let data = async () => {
+      let response = await FetchMe.Me(token);
+      if (response) {
+      }
+    };
+    data();
+  }, []);
+
   return (
     <div>
       <h1>Me</h1>
