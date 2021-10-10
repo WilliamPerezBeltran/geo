056c57fe7e7ae0c55b6ee8f37ea7d1c212d82f46
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-10-10 08:57:38 -0500

    Simulate data in the Me component

--- src/components/Me.js
+++ src/components/Me.js
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import { useStore } from "react-redux";
 import * as FetchMe from "../services/FetchPersonalData.js";
 
+import * as obj from "../data/DataMe"
 export const Me = () => {
   const store = useStore();
   const token = store.getState().reducer_token.token.access_token;
 
   useEffect(() => {
     let data = async () => {
+      console.log(obj.personalData)
       let response = await FetchMe.Me(token);
       if (response) {
       }
---
+++ src/data/DataMe.js
@@ -0,0 +1,12 @@
+ export const personalData = {
+    id: 1,
+    name: "Roxanne Weissnat Sr.",
+    email: "tvandervort@example.net",
+    email_verified_at: "2020-10-02T14:49:58.000000Z",
+    created_at: "2020-10-02T14:49:58.000000Z",
+    updated_at: "2020-10-02T14:49:58.000000Z",
+  };
+
+  export default  {
+  	personalData
+  }
